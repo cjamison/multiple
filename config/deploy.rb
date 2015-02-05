29727e7d429ef3a0c8b2e52a5d36de06a85a9ff4
Creston Jamison <creston@rubytreesoftware.com>
2015-02-05 15:56:07 -0500

    Set unicorn control for only the web role.

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,7 +42,7 @@ namespace :deploy do
   %w[start stop restart].each do |command|
     desc 'Manage Unicorn'
     task command do
-      on roles(:app), in: :sequence, wait: 1 do
+      on roles(:web), in: :sequence, wait: 1 do
         execute "/etc/init.d/unicorn_#{fetch(:application)} #{command}"
       end      
     end
